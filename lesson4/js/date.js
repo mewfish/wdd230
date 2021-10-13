b9b6deb6305e86180a4156ab0662dc078e294861
mewfish <rebeccabest04@gmail.com>
2021-10-12 18:17:39 -0600

    jjj

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,15 +1,6 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-/*** Programming Notes **************************************
-  Arrow Functions - es6 syntactically compact alternative to a regular function expression
-  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-  or https://www.w3schools.com/js/js_arrow_function.asp
-
-  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  */
+// let dt = new Date();
+// document.getElementById("date").innerHTML = dt.toLocaleDateString();
+options.timeZone = 'UTC';
+options.timeZoneName = 'short';
+console.log(date.toLocaleDateString('en-US', options));
+// → "Thursday, December 20, 2012, UTC"
